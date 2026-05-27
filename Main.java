22e93008130829a6732163d2e585fcbfb3cfb06d
mrawesome10 <ahmedalharazi369@gmail.com>
2026-05-27 22:52:49 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Inventory pharmacy = new Inventory();
+        Medication med = new Medication("D6T6", "Painkiller", 4000, 100, "12/2027");
+        MedicalDevices equip = new MedicalDevices("E202", "Thermometer", 4000, 15, 24);
+        pharmacy.addItem(med);
+        pharmacy.addItem(equip);
+        pharmacy.showInventory();
+    }
+} 
+
